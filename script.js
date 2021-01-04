687c56b41e192ad2b8fac087053a0105ce23bb3c
Sadman <skc86@cornell.edu>
2021-01-04 16:56:51 -0500

    added the javascript so that the tip amount is calculated once inputs are put in

---
+++ script.js
@@ -0,0 +1,22 @@
+function findTip() {
+  var bill = document.getElementById("amount").value;
+  var number_of_people = document.getElementById("number-of-people").value;
+  var quality = document.getElementById("service-quality").value;
+
+  // Check if they put in the right inputs
+  if (quality == 0 || bill == "" || number_of_people == "") {
+    alert("Please Enter All Values!");
+  }
+
+  //Find tip amount
+  var tip = (bill * quality) / number_of_people;
+  tip = tip.toFixed(2);
+
+  //set it to display in the page
+  document.getElementById("tip").innerHTML = "$ " + tip.toString();
+}
+
+// function is called once the button is clicked
+document.getElementById("Calculate").onclick = function () {
+  findTip();
+};
